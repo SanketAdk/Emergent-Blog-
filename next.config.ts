d673cf895602fa5f316e3f431c97e086809fcdd9
SanketAdk <sanketadhikari149@gmail.com>
2026-07-26 08:26:44 +0545

    "Fix: Configure Next.js for static files"

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  staticPageGenerationTimeout: 60,
+  poweredByHeader: false,
+  compress: true,
 };
 
 export default nextConfig;
